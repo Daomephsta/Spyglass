17825a375e7993cba8248677556eba075b790125
Daomephsta <dtnilfisk@gmail.com>
2019-07-21 13:25:13 +1200

    Update Fabric version

--- src/main/java/io/github/daomephsta/spyglass/SpyglassInitialiser.java
+++ src/main/java/io/github/daomephsta/spyglass/SpyglassInitialiser.java
@@ -1,23 +1,20 @@
 package io.github.daomephsta.spyglass;
 
 import com.mojang.brigadier.CommandDispatcher;
-
 import io.github.daomephsta.spyglass.command.SpyglassCommand;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.server.ServerStartCallback;
 import net.minecraft.server.command.ServerCommandSource;
 
-public class SpyglassInitialiser implements ModInitializer
+public class SpyglassInitialiser
 {
 	public static final String MOD_ID = "spyglass";
-	
-	@Override
-	public void onInitialize()
+
+	public static void initialise()
 	{
 		ServerStartCallback.EVENT.register(server -> registerCommands(server.getCommandManager().getDispatcher()));
 	}
 
-	private void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher)
+	private static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher)
 	{
 		SpyglassCommand.register(dispatcher);
 	}
